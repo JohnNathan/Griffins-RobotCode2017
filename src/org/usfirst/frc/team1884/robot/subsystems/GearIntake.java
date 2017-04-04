76fdc817418384bb3c3ff50e689b478997eb578f
Jonathan Wang <jwang@nikroulah.local>
2017-04-04 10:55:41 +0100

    First Commit

---
+++ src/org/usfirst/frc/team1884/robot/subsystems/GearIntake.java
@@ -0,0 +1,85 @@
+package org.usfirst.frc.team1884.robot.subsystems;
+
+import org.usfirst.frc.team1884.robot.Robot;
+import org.usfirst.frc.team1884.robot.commands.IntakeHoldCommand;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+/**
+ *
+ */
+public class GearIntake extends Subsystem implements Debuggable, Recordable {
+
+	private VictorSP rollerMotor, liftMotor;
+	private DigitalInput topLimit, botLimit;
+	
+	public GearIntake() {
+		rollerMotor = new VictorSP(Robot.Map.IN_ROLLER);
+		liftMotor = new VictorSP(Robot.Map.IN_LIFT);
+		
+		topLimit = new DigitalInput(Robot.Map.IN_TOP_LIM) { @Override public boolean get() { return !super.get(); } };
+		botLimit = new DigitalInput(Robot.Map.IN_BOT_LIM) { @Override public boolean get() { return !super.get(); } };
+	}
+	
+	public void up() {
+		liftMotor.set(topLimit.get() ? 0.0 : 0.37);
+	}
+	public void down() {
+		liftMotor.set(botLimit.get() || Robot.InstanceMap.gearMan.isUp() ? 0.0 : -0.1);
+	}
+	public void downHarder() {
+		liftMotor.set(botLimit.get() ? 0.0 : -0.4);
+	}
+	public void hold() {
+		liftMotor.set(.10);
+	}
+	public void stop() {
+		liftMotor.set(0.0);
+	}
+	public void intake() {
+		rollerMotor.set(1.0);
+	}
+	public void outtake() {
+		rollerMotor.set(-1.0);
+	}
+	public void rollerOff() {
+		rollerMotor.set(0.0);
+	}
+	public boolean isAtTop() {
+		return topLimit.get();
+	}
+	public boolean isAtBot() {
+		return botLimit.get();
+	}
+    public void initDefaultCommand() {
+    	setDefaultCommand(new IntakeHoldCommand());
+    }
+    @Override
+    public void updateSmartDashboard() {
+    	SmartDashboard.putBoolean("IN Top Switch", this.isAtTop());
+    	SmartDashboard.putBoolean("IN Bot Switch", this.isAtBot());
+    }
+    // arm direction(-1=down,0=hold,1=up), intake(1=in,-1=out)
+    @Override
+    public double[] getData() {
+    	return new double[]{liftMotor.get() > 0.0 ? 1.0 : liftMotor.get() < 0.0 ? -1.0 : 0.0,
+    			rollerMotor.get() > 0.0 ? 1 : rollerMotor.get() < 0 ? -1.0 : 0.0};
+    }
+    // arm direction(-1=down,0=hold,1=up), intake(1=in,-1=out)
+    @Override
+    public void putData(double[] data) {
+    	if (data.length != 2) return;
+    	double lift = data[0];
+    	double intake = data[1];
+    	if (lift < 0) this.down();
+    	else if (lift > 0) this.up();
+    	else this.stop();
+    	if (intake > 0) this.intake();
+    	else if (intake < 0) this.outtake();
+    	else this.rollerOff();
+    }
+}
--- src/org/usfirst/frc/team1884/robot/subsystems/GearManipulator.java
+++ src/org/usfirst/frc/team1884/robot/subsystems/GearManipulator.java
@@ -1,15 +1,18 @@
 package org.usfirst.frc.team1884.robot.subsystems;
 
 import org.usfirst.frc.team1884.robot.Robot;
+import org.usfirst.frc.team1884.robot.commands.GearManDefault;
 import org.usfirst.frc.team1884.util.DoubleSolenoidWrapper;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
-public class GearManipulator extends Subsystem {
+public class GearManipulator extends Subsystem implements Debuggable, Recordable {
 
 	private DoubleSolenoidWrapper claw, linkage;
 	private final static long claw_timer = 250L, linkage_timer = 800L;
@@ -20,17 +23,28 @@ public class GearManipulator extends Subsystem {
 			k_claw_open = DoubleSolenoid.Value.kForward,
 			k_off = DoubleSolenoid.Value.kOff;
 	
+	private DigitalInput gearSwitch;
+	
     public GearManipulator() {
     	claw = new DoubleSolenoidWrapper(Robot.Map.GM_CLAW_F, Robot.Map.GM_CLAW_R, claw_timer);
     	linkage = new DoubleSolenoidWrapper(Robot.Map.GM_LINK_F, Robot.Map.GM_LINK_R, linkage_timer);
+    	
+    	this.closeClaw();
+    	this.lowerClaw();
+    	
+    	gearSwitch = new DigitalInput(Robot.Map.GM_SWITCH);
     }
     
     public void liftClaw() {
-    	linkage.set(k_linkage_up);
+    	if (Robot.InstanceMap.intake.isAtTop() && claw.getActualState() == k_claw_closed) {
+    		linkage.set(k_linkage_up);
+    	}
     }
     
     public void lowerClaw() {
-    	linkage.set(k_linkage_down);
+    	if (claw.getActualState() == k_claw_closed) {
+    		linkage.set(k_linkage_down);
+    	}
     }
     
     public void openClaw() {
@@ -45,10 +59,54 @@ public class GearManipulator extends Subsystem {
     	linkage.update();
     	claw.update();
     }
+    public boolean isGearIn() {
+    	return gearSwitch.get();
+    }
+    public boolean isUp() {
+    	return linkage.getActualState() == k_linkage_up;
+    }
+    public boolean finishedLinkage() {
+    	return linkage.finishedExtension();
+    }
+    public boolean finishedClaw() {
+    	return claw.finishedExtension();
+    }
 
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+        setDefaultCommand(new GearManDefault());
     }
+
+	@Override
+	public void updateSmartDashboard() {
+		SmartDashboard.putString("GM Claw State", claw.getActualState() == k_claw_closed ? "CLOSED" : "OPEN");
+	}
+	
+	// clawClosed(0=close,1=open), clawUp(1=up,0=down)
+	@Override
+	public double[] getData() {
+		return new double[]{claw.getActualState() == k_claw_closed ? 1.0 : 0.0,
+				linkage.getActualState() == k_linkage_up ? 1.0 : 0.0};
+	}
+	
+	// claw state (0=close,1=open), linkage state (0=down,1=up)
+	private double prevClawState = 0, prevLinkageState = 0;
+	@Override
+	public void putData(double[] data) {
+		if (data.length != 2) return;
+		double clawState = data[0];
+		double linkageState = data[1];
+
+		if (clawState != prevClawState) {
+			if (clawState == 0.0) this.closeClaw();
+			else if (clawState == 1.0) this.openClaw();
+		}
+		if (linkageState != prevLinkageState) {
+			if (clawState == 0.0) this.lowerClaw();
+			else if (clawState == 1.0) this.liftClaw();
+		}
+
+		prevClawState = clawState;
+		prevLinkageState = linkageState;
+	}
 }
 
